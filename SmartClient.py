6d10444351dcf582b21d740907812b92c5b3c2b9
Joss Vrooman <jvrooman@linux5.csc.uvic.ca>
2018-01-20 15:41:31 -0800

    basic connection functionality

--- SmartClient.py
+++ SmartClient.py
@@ -1,5 +1,14 @@
+#!/usr/bin/python3
+
 import sys
 import ssl
 import socket
+#import hpack?
 
 print(sys.argv)
+
+s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+s.connect(("www.python.org", 80))
+s.sendall("GET http://www/python/org HTTP/1.0 \n\n")
+print s.recv(4096)
+s.close()
