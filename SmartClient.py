f5805482391dfc718682a9555705a1f4926fdb1d
Joss Vrooman <jvrooman@linux4.csc.uvic.ca>
2018-01-23 12:06:43 -0800

    added checking the given command line paramaters

--- SmartClient.py
+++ SmartClient.py
@@ -1,14 +1,30 @@
-#!/usr/bin/python3
-
+#.recv(4096)!/usr/bin/python3
+import re
 import sys
 import ssl
 import socket
 #import hpack?
 
-print(sys.argv)
+if(len(sys.argv) < 2):
+	print("Please add url as command line argument")
+	sys.exit()
 
+url = sys.argv[1]
+matchhttp = "^http://"
+matchhttps = "^https://"
+urlstatus = 0
+
+if (re.match(matchhttp, url)):
+	urlstatus = 1
+elif(re.match(matchhttps, url)):
+	urlstatus = 2
+
+print(urlstatus)	
+
+'''
 s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
 s.connect(("www.python.org", 80))
 s.sendall("GET http://www/python/org HTTP/1.0 \n\n")
 print s.recv(4096)
 s.close()
+'''
