35eba8ebb53efc21049eb53b458a3bddedf75dc1
Joss Vrooman <jvrooman@n-lambda.csc.uvic.ca>
2018-01-24 14:15:21 -0800

    added basic https and finding of status code and version

--- SmartClient.py
+++ SmartClient.py
@@ -1,30 +1,73 @@
-#.recv(4096)!/usr/bin/python3
+#!/usr/bin/python3
 import re
 import sys
 import ssl
 import socket
 #import hpack?
 
-if(len(sys.argv) < 2):
+def getInfo(info):
+	match = re.search("HTTP/([12].[01]) (\d\d\d)", info)
+	if(match):
+		print(match.group(1))
+		print(match.group(2))
+		return match.group(1), match.group(2)
+	else:
+		print("yikes")
+
+'''
+def parseUrl(url):
+	matchhttp = "^http://" #regular expression to check what is at the beggingin of the url
+	matchhttps = "^https://" #as above
+	urlstatus = 0 #0 is nothing, 1 is http://, 2 is https://
+
+	if (re.match(matchhttp, url)): #check if http at begginging of url
+		urlstatus = 1
+	elif(re.match(matchhttps, url)): #check if https at begginging of url
+		urlstatus = 2
+	return url, urlstatus
+'''
+
+def connectHTTPS(url):
+	print("connectHTTPS")
+	s = socket.socket(socket.AF_INET, socket.SOCK_STREAM, 0)
+	ssls = ssl.wrap_socket(s, ssl_version = ssl.PROTOCOL_SSLv23)
+	IP = socket.gethostbyname(url)
+	ssls.connect((IP, 443))
+	ssls.write(("GET / HTTP/1.1\r\nHost: " + url +"\r\nConnection: close\r\n\r\n").encode('latin-1'))
+	responce = ssls.recv(4096).decode('latin-1')
+	ssls.close()
+	return responce
+
+def connectHTTP(url):
+	print("connectHTTP")
+'''
+def connect(url, port):
+
+	print("connect")
+	s = socket.socket(socket.AF_INET, socket.SOCK_STREAM, 0)
+	ssls = ssl.wrap_socket(s, ssl.PROTOCOL_TLSv1, "ADH-AES256-SHA")
+	IP = socket.gethostbyname(url)
+	ssls.connect((IP, 443))
+	ssls.sendall("HEAD https://" + url + "  HTTP/1.1 \n\n")
+	print ssls.recv(4096)
+
+	ssls.close()
+
+	s = socket.socket(socket.AF_INET, socket.SOCK_STREAM, 0)
+	s.connect((url, port))
+	s.sendall("HEAD http://" + url + "  HTTP/1.1 \n\n")
+	print s.recv(4096)
+
+	s.close()
+'''
+
+if(len(sys.argv) < 2): #make sure that we are given a command line argument
 	print("Please add url as command line argument")
 	sys.exit()
 
+#url, urlstatus = parseUrl(sys.argv[1]) #the command line argument should be a url
 url = sys.argv[1]
-matchhttp = "^http://"
-matchhttps = "^https://"
-urlstatus = 0
-
-if (re.match(matchhttp, url)):
-	urlstatus = 1
-elif(re.match(matchhttps, url)):
-	urlstatus = 2
-
-print(urlstatus)	
-
-'''
-s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-s.connect(("www.python.org", 80))
-s.sendall("GET http://www/python/org HTTP/1.0 \n\n")
-print s.recv(4096)
-s.close()
-'''
+print(url)
+results = connectHTTPS(url)
+print(results)
+getInfo(results)
