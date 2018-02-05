392975d197e202a108127e5f23a51908fa703744
Joss Vrooman <Joss Vrooman>
2018-02-05 15:23:00 -0800

    final version, didn't quite finish everything, but oh well on to the next assignment

--- SmartClient.py
+++ SmartClient.py
@@ -1,37 +1,31 @@
 #!/usr/bin/python3
+#coded on python 3.6.4
+
+#Author: Joss Vrooman, V00813897
+#CSC361 Assignment 1
+
 import re
 import sys
 import ssl
 import socket
-#import h2
-#import hpack?
-
-#NOTES
-#
-#Need to handle statues codes: 505, 404, 301, 302, 200
 
 #--General----------------------------------------------------------------------------------------------------------------------------------------------------------
 
-#if we need to connect somewhere else
+#on redirect find info on https vs http and the status code
 def getNewLocation(info):
-	match = re.search("Location: (https?)://(.*)/", info)
+	match = re.search("Location: (https?)://([^/]*)/", info)
 	if(match):
-		print("new location:" + match.group(1) + " " + match.group(2))
 		return match.group(1), match.group(2)
 	else:
-		print("yikes2")
+		print("Error in getNewLocation")
 
 #fetches the response code and the HTTP version from the response
 def getInfo(info):
 	match = re.search("HTTP/([12].[01]) (\d\d\d)", info)
 	if(match):
-		print("new Info:" + match.group(1) + " " + match.group(2))
 		return match.group(1), match.group(2)
 	else:
-		print("yikes")
-
-def connectHTTP(url):
-	print("connectHTTP")
+		print("Error in getInfo")
 
 #--HTTPS----------------------------------------------------------------------------------------------------------------------------------------------------------
 
@@ -40,14 +34,13 @@ def findHTTPS(url):
 	try:
 		for i in range(10):
 			results = connectHTTPS(url) 
-			#print(results)#//
 			version, reCo = getInfo(results) #finds the http version and response code
-			if (reCo == '302' or reCo == '301'): #on redirect code
+			if (reCo == '302' or reCo == '301' or reCo == '505'): #on redirect code
 				list = getNewLocation(results)#find redirect
 				url = list[1]#and try again
 			elif(reCo == '200'):#on okay we know they support https
 				return True
-			else:
+			else:#on error
 				print("Unexpected response code: " + reCo) 
 				sys.exit()
 	except:
@@ -56,11 +49,13 @@ def findHTTPS(url):
 
 #try to connect https 1.1
 def connectHTTPS(url):
-	print("connectHTTPS")
+	#build socket
 	s = socket.socket(socket.AF_INET, socket.SOCK_STREAM, 0)
+	#wrap soccket
 	ssls = ssl.wrap_socket(s, ssl_version = ssl.PROTOCOL_SSLv23)
 	IP = socket.gethostbyname(url)
 	ssls.connect((IP, 443))
+	#send request
 	ssls.write(("GET / HTTP/1.1\r\nHost: " + url +"\r\nConnection: close\r\n\r\n").encode('latin-1'))
 	responce = ssls.recv(4096).decode('latin-1')
 	ssls.close()
@@ -68,12 +63,24 @@ def connectHTTPS(url):
 
 #--HTTP Version----------------------------------------------------------------------------------------------------------------------------------------------------------
 
-def findVersion(url):
-	try:
+#connect on HTTP on given version
+def connectHTTP(url, version):
+	#build socket
+	s = socket.socket(socket.AF_INET, socket.SOCK_STREAM, 0)
+	s.connect((url, 80))
+	#send request
+	s.sendall(("GET / HTTP/"+version+ "\r\nHost: " + url +"\r\nConnection: close\r\n\r\n").encode('latin-1'))
+	responce = s.recv(4096).decode('latin-1')
+	s.close()
+	return responce	
+
+#finds highest HTTP version to use	
+def findVersion(url, httpsStatus):
+	try: #try http 2.0
 		for i in range(10):
 			results = connectHTTPS(url)
 			version, reCo = getInfo(results) #finds the http version and response code
-			if (reCo == '302' or reCo == '301'): #on redirect code
+			if (reCo == '302' or reCo == '301' or reCo == '505'): #on redirect code
 				list = getNewLocation(results)#find redirect
 				url = list[1]#and try again
 				continue
@@ -82,21 +89,111 @@ def findVersion(url):
 				sys.exit()
 			
 			if(checkHTTP2(url)):
-				return(2.0)
+				return('2.0')
 			else:
 				break
-	except Exception as e:
-		print(e)
-		print("oops")
+	except:
+		pass
+		#print("Error in findVersion: 2.0")
+	
+	try:#try HTTP 1.1
+		for i in range(10):
+			if(httpsStatus):
+				results = connectHTTPS(url)
+			else:
+				results = connectHTTP(url, '1.1')
+			version, reCo = getInfo(results)
+			if (reCo == '302' or reCo == '301' or reCo == '505'): #on redirect code
+				list = getNewLocation(results)#find redirect
+				url = list[1]#and try again
+				continue
+			elif(reCo != '200'):#
+				print("Unexpected response code: " + reCo) 
+				sys.exit()
+			if(version == '1.1'):
+				return '1.1'
+	except:
+		pass
+		#print("Error in findVersion: 1.1")
 		
+	try:#try HTTP 1.0
+		for i in range(10):
+			results = connectHTTP(url, '1.0')
+			version, reCo = getInfo(results)
+			if (reCo == '302' or reCo == '301' or reCo == '505'): #on redirect code
+				list = getNewLocation(results)#find redirect
+				url = list[1]#and try again
+				continue
+			elif(reCo != '200'):#
+				print("Unexpected response code: " + reCo) 
+				sys.exit()
+			if(version == '1.0'):
+				return '1.0'
+		print("To many redirects")
+	except:
+		pass
+		#print("Error in findVersion: oops3")	
+		
+	return "Unable to connect and determine HTTP version"
 	
 
 #--Cookies----------------------------------------------------------------------------------------------------------------------------------------------------------
 
+#gets cookies from spot depending on if server accepts
+#https and http version
+def getCookies(url, httpsStatus, version):
+	if(httpsStatus):#https
+		for i in range(10):
+			results = connectHTTPS(url) 
+			version, reCo = getInfo(results) #finds the http version and response code
+			if (reCo == '302' or reCo == '301'): #on redirect code
+				list = getNewLocation(results)#find redirect
+				url = list[1]#and try again
+			elif(reCo == '200'):#on okay we know they support https
+				break
+			else:
+				print("Unexpected response code: " + reCo) 
+				sys.exit()
+	else:#http
+		for i in range(10):
+			results = connectHTTP(url, version)
+			version, reCo = getInfo(results)
+			if (reCo == '302' or reCo == '301'): #on redirect code
+				list = getNewLocation(results)#find redirect
+				url = list[1]#and try again
+				continue
+			elif(reCo == '200'):#
+				break;
+			elif(reCo == '505'):
+				version = '1.0'
+			else:
+				print("Unexpected response code: " + reCo) 
+				sys.exit()
+	
+	
+	#return only the stuff we want
+	match = re.findall("Set-Cookie: (.*)", results)
+	
+	return match
 
+#find the name and key of a cookie
+def parseNameKey(cookie):
+	match = re.search("([^=]*)=([^;]*);", cookie)
+	return match.group(1), match.group(2)
 
-#--H2?----------------------------------------------------------------------------------------------------------------------------------------------------------
+#find the domain of a cookie
+#note that not every cookie has a domain
+#so its in a seperate function
+def parseDomain(cookie):
+	match = re.search("[Dd]omain=(.*);?", cookie)
+	return match.group(1)
 		
+#--HTTP2----------------------------------------------------------------------------------------------------------------------------------------------------------
+#PLEASE NOTE:
+#This is not my work anything below this until the main
+#method was taken from
+#https://python-hyper.org/projects/h2/en/stable/negotiating-http2.html
+#and all credit should go to them for this code	
 def negotiate_tls(tcp_conn, context, url):
     """
     Given an established TCP connection and a HTTP/2-appropriate TLS context,
@@ -177,39 +274,36 @@ def get_http2_ssl_context():
     return ctx
 
 #--MAIN---------------------------------------------------------------------------------------------------------------------------------------------------------
-	
+#The following is my (Joss Vrooman) code	
 if(len(sys.argv) < 2): #make sure that we are given a command line argument
 	print("Please add url as command line argument")
-	sys.exit()
+	sys.exit() 	
 
-url = sys.argv[1]
-print(url)
-if(findHTTPS(url)):
-	print("HTTPS true")
+url = sys.argv[1]#url
+httpsStatus = findHTTPS(url)#find if HTTPS is supported
+print("Website: " + url)
+if(httpsStatus):
+	print("Support of HTTPS: True")
 else:
-	print("HTTPS false")
-if(findVersion(url)):
-	print("H2 TRUE")
-else:
-	print("H2 FALSE")
-
-'''
-def connect(url, port):
-
-	print("connect")
-	s = socket.socket(socket.AF_INET, socket.SOCK_STREAM, 0)
-	ssls = ssl.wrap_socket(s, ssl.PROTOCOL_TLSv1, "ADH-AES256-SHA")
-	IP = socket.gethostbyname(url)
-	ssls.connect((IP, 443))
-	ssls.sendall("HEAD https://" + url + "  HTTP/1.1 \n\n")
-	print ssls.recv(4096)
-
-	ssls.close()
-
-	s = socket.socket(socket.AF_INET, socket.SOCK_STREAM, 0)
-	s.connect((url, port))
-	s.sendall("HEAD http://" + url + "  HTTP/1.1 \n\n")
-	print s.recv(4096)
-
-	s.close()
-'''
+	print("Support of HTTPS: False")
+version = findVersion(url, httpsStatus)#find the HTTP version
+print("HTTP Version: HTTP/" + version)
+#if HTTP is supported find cookies
+if(version == '2.0' or version == '1.1' or version == '1.0'):
+	#find cookies
+	cookies = getCookies(url, httpsStatus, version)
+	print("List of Cookies:")
+	#for each cookie find the name key and domain 
+	#and make it look nice
+	for i in cookies:
+		name, key = parseNameKey(i)
+		domain = "";
+		try:
+			domain = parseDomain(i)
+		except:
+			pass
+		if(name and key and domain):
+			print("Name: " + name + "; Key: " + key + "; Domain: " + domain)
+		else:
+			print("Name: " + name + "; Key: " + key)
+	
